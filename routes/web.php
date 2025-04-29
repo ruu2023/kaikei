c4bea0509a7e6e58d9645ccac79fe23aa922ceb5
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2025-04-29 17:39:02 +0900

    Revert "delete app.css root to dashboard"

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,14 @@ use App\Http\Controllers\AppController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/dashboard');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+
+    // return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [AppController::class, 'dashboard'])->name('dashboard');
