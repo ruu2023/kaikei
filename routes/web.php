acd0cb7afbd99ee5deca3939e26ce8c6df450431
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2025-04-29 17:09:27 +0900

    delete app.css root to dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,7 @@ use App\Http\Controllers\AppController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-
-    // return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::redirect('/', '/dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [AppController::class, 'dashboard'])->name('dashboard');
