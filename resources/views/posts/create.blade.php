6f0c1a99277387e6bcef40a3e487f961f58ff20d
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2025-04-29 15:21:03 +0900

    reset Breeze layout

--- resources/views/posts/create.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <h1>新規投稿確認用です。</h1>
-    </div>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +12,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::resource('posts', PostController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
