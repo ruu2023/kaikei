801400e9582d7b64ad93f8f021f117bde1b6cf22
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2025-06-09 00:42:52 +0900

    fix: analytics.js load

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,17 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                "resources/sass/app.scss",
-                "resources/sass/analytics.scss",
-                "resources/js/app.js",
-                "resources/js/transaction.js",
-                "resources/js/settings.js",
-            ],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        "resources/sass/app.scss",
+        "resources/sass/analytics.scss",
+        "resources/js/app.js",
+        "resources/js/transaction.js",
+        "resources/js/settings.js",
+        "resources/js/analytics.js",
+      ],
+      refresh: true,
+    }),
+  ],
 });
